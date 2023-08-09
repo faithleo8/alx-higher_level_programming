3a5b4483976e3cdcf1a5dd7ead4a0630372045f1
faithleo8 <leofaith8@gmail.com>
2023-08-09 19:12:52 +0300

    alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for char in range(97, 123):
-if char == ord('e') or char == ord('q'):
-continue
-print("{}".format(chr(char)), end='')
+    if char == ord('e') or char == ord('q'):
+        continue
+    print("{}".format(chr(char)), end='')
