08d0db8ea97b27425c3e05473c1318460cbd8bd3
faithleo8 <leofaith8@gmail.com>
2023-08-09 18:38:40 +0300

    1

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-for char in range(97, 123):
-        if char == ord('e') or char == ord('q'):
-                    continue
-                    print("{}".format(chr(char)), end='')
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-for decimal in range(99):
-        print("{} = {}".format(decimal, hex(decimal)))
