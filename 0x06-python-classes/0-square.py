6e3d5264c1e8bc7cadc0209d61d78ca1174e6837
faithleo8 <leofaith8@gmail.com>
2023-08-29 07:17:28 -0700

    files

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" this module contains the square class"""
+
+
+class Square:
+    """
+    Represents a square.
+
+    This class provides a basic representation of a square.
+
+    Attributes:
+        None
+
+    Methods:
+        __init__(): Constructor for the Square class.
+    """
+
+    def __init__(self):
+        """
+        Constructor for Square class.
+
+        Initializes a new instance of the Square class.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
+        pass
