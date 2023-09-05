26210e1b5234c3287ca98f9dc213a7cb68b05ec4
faithleo8 <leofaith8@gmail.com>
2023-09-05 07:38:06 -0700

    objects

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    module for a class which is locked
+"""
+
+
+class LockedClass:
+    """
+        class with a locked attribute first name
+    """
+    __slots__ = "first_name"
